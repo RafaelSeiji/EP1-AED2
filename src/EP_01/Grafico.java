d2241fad1439fc214559e500547b2bb2fbca2614
GabrielBMoreira <gabrielbrandaorj@hotmail.com>
2020-04-07 18:34:57 -0300

    Atualização

--- src/EP_01/Grafico.java
+++ src/EP_01/Grafico.java
@@ -1,4 +1,34 @@
 package EP_01;
 
-public class Grafico {
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+
+public class Grafico extends ApplicationFrame {
+
+    protected Grafico(String tituloApp, String tituloGrafico){
+        super(tituloApp);
+        JFreeChart barChart = ChartFactory.createBarChart(tituloGrafico,"IDs","Valores",
+                                                          createDataset(), PlotOrientation.VERTICAL, true, true, false);
+        ChartPanel chartPanel = new ChartPanel(barChart);
+        chartPanel.setPreferredSize(new java.awt.Dimension(560, 367));
+        setContentPane(chartPanel);
+    }
+
+    protected CategoryDataset createDataset(Lista lista){
+        final DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        if(lista.inicio == null) return null;
+        else{
+            No aux = lista.inicio;
+            while (aux.prox != null){
+                dataset.addValue();
+            }
+        }
+    }
+
 }
